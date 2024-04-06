607b6a4fc72ffe3c44d2a6df18c9b97b3e725af7
Tolu A <tolu@adesegha.ca>
2024-04-06 11:00:07 -0600

    Initial Commit

---
+++ The Final Countdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Final Countdown
+//
+//  Created by Tolu A on 2024-04-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Final Countdown/The_Final_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Final_CountdownApp.swift
+//  The Final Countdown
+//
+//  Created by Tolu A on 2024-04-06.
+//
+
+import SwiftUI
+
+@main
+struct The_Final_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
